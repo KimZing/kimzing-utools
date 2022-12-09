b39224024569cebbd8acece384c6941a164da21f
kimzing <kimzing@163.com>
2022-12-09 15:02:31 +0800

    跟新环境变量

--- jetbrains-toolbox/utils/shell.js
+++ jetbrains-toolbox/utils/shell.js
@@ -1,9 +1,15 @@
 const {exec} = require("child_process");
 
 function execute(command) {
-    exec(command, (err) => {
-        if (err) utools.showNotification(err.message);
-    })
+    if (utools.isMacOS()) {
+        exec(`PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin;${command}`, (err) => {
+            if (err) utools.showNotification(err.message);
+        })
+    } else {
+        exec(`${command}`, (err) => {
+            if (err) utools.showNotification(err.message);
+        })
+    }
 }
 
 module.exports = {
