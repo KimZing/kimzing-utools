063fda833075216f2fbfe05e6c4bc9acefc81677
KimJin <kim.jin@batechworks.com>
2024-03-04 18:04:11 +0800

    添加默认使用 zsh 环境

--- jetbrains-toolbox/utils/shell.js
+++ jetbrains-toolbox/utils/shell.js
@@ -1,10 +1,17 @@
-const {exec} = require("child_process");
+const { exec } = require("child_process");
+const {exists, userHome} = require("./file");
 
 function execute(command) {
     if (utools.isMacOS()) {
-        exec(`PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin;${command}`, (err) => {
-            if (err) utools.showNotification(err.message);
-        })
+        if (exists('/bin/zsh') && exists(`${userHome()}/.zshrc`)) {
+            exec(`PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin;${command}`, { shell: '/bin/zsh' }, (err) => {
+                if (err) utools.showNotification(err.message);
+            })
+        } else {
+            exec(`PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin;${command}`, (err) => {
+                if (err) utools.showNotification(err.message);
+            })
+        }
     } else {
         exec(`${command}`, (err) => {
             if (err) utools.showNotification(err.message);
